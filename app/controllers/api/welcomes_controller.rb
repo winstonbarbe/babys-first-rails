173d9acc81887fb39ffb2b9a3d871c1e3a0d0ada
winstonbarbe <winston.barbe@gmail.com>
2020-09-08 20:24:39 -0400

    Adds about path/controller

--- app/controllers/api/welcomes_controller.rb
+++ app/controllers/api/welcomes_controller.rb
@@ -4,4 +4,9 @@ class Api::WelcomesController < ApplicationController
     @random = Random.new.rand(100)
     render 'hello.json.jb'
   end
+
+  def about
+    @language = "binary"
+    render 'about.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/welcome_path" => "welcomes#hello"
+    get "/about_path" => "welcomes#about"
   end
 
 end
